70a8a040c3f239044514d12b5a11ca8af5d014f0
Dheeban SG <sgdheeban@gmail.com>
2024-12-04 02:13:54 -0800

    adding struct

--- projects/hello_cargo/src/main.rs
+++ projects/hello_cargo/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
+/*fn main() {
     println!("Hello, world 2!");
+}*/
+
+struct Rectangle {
+    height1: u32,
+    width1: u32,
+}
+
+fn main() {
+    let rect = Rectangle {
+        height1: 30,
+        width1: 50,
+    };
+
+    println!( "{}", 
+        area(&rect)
+    );
+
+    println!("h: {0}, w: {1}", rect.height1, rect.width1);
+
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width1*rectangle.height1
 }
