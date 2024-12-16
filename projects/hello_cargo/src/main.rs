0d70910189ddc840fc400a7f153d74231f7b13bc
Dheeban SG <sgdheeban@gmail.com>
2024-12-16 14:44:42 -0800

    match

--- projects/hello_cargo/src/main.rs
+++ projects/hello_cargo/src/main.rs
@@ -1,7 +1,23 @@
+use std::i32;
+
 #[derive(Debug)]
 enum IPAddrKind {
     V4,
     V6,
+}
+
+#[derive(Debug)]
+enum UsState {
+    Texas(String, i32),
+    Washington(String, i32),
+    // --- Snip ---
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime, 
+    Quater(UsState),
 }
 
 #[derive(Debug)]
@@ -34,10 +50,70 @@ impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.height1 > other.height1 && self.width1 > other.width1
     }
+
 }
 
 fn route(ip:IPAddrKind) {
     println!("{:#?}",ip);
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky Penny!!!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quater(UsState::Texas(city, population)) => {
+            println!("Quarter got attached to the state - {:#?}, {:#?}", city, population);
+            25
+        },
+        Coin::Quater(UsState::Washington(city, population)) => {
+            println!("Quarter got attached to the state - {:#?}, {:#?}", city, population);
+            25
+        },
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i+1) 
+    }
+}
+
+fn put_on_fancy_hat() {
+    println!("Put on fancy hat");
+}
+
+fn remove_fancy_hat() {
+    println!("Remove fancy hat");
+}
+
+fn move_to_other_player(x: i32) {
+    println!("Moving to other player - {x}");
+}
+
+
+fn re_roll() {
+    println!("Re-rolling")
+}
+
+fn roll_dice(x: i32) {
+    match x {
+        3 => put_on_fancy_hat(),
+        7 => remove_fancy_hat(),
+        other => move_to_other_player(other)
+    }
+}
+
+fn roll_dice_2(x: i32) {
+    match x {
+        3 => put_on_fancy_hat(),
+        7 => remove_fancy_hat(),
+        _ => re_roll(),
+    }
 }
 
 fn main() {
@@ -84,6 +160,27 @@ fn main() {
 
     route(IPAddrKind::V4);
     route(IPAddrKind::V6);
+
+    println!("Sort coin {}", value_in_cents(Coin::Penny));
+    println!("Sort coin {}", value_in_cents(Coin::Nickel));
+    println!("Sort coin {}", value_in_cents(Coin::Dime));
+    println!("Sort coin {}", value_in_cents(Coin::Quater(UsState::Washington(String::from("WA"), 98012))));
+    println!("Sort coin {}", value_in_cents(Coin::Quater(UsState::Texas(String::from("TX"), 78540))));
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    println!("Six {:#?}", six);
+    println!("Seven {:#?}", plus_one(Some(6)));
+
+    println!("Six {:#?}", none);
+    println!("None 2 {:#?}", plus_one(None));
+
+    roll_dice(3);
+    roll_dice(7);
+    roll_dice(9);
+    roll_dice_2(9);
 
 }
 
