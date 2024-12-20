70ec0685ce64553250f8e71b00243751f2b5957f
Dheeban SG <sgdheeban@gmail.com>
2024-12-20 15:41:38 -0800

    bin, lib crate

--- projects/hello_packages/restaurant/src/lib.rs
+++ projects/hello_packages/restaurant/src/lib.rs
@@ -1,16 +1,26 @@
-// Grouping related code into modules
-mod front_of_house {
-    mod hosting {
-        fn add_to_wait_list() {}
 
-        fn seat_at_table() {}
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_wait_list(relative_path: bool) {
+            println!("add_to_wait_list via relative path: {}", relative_path);
+        }
+
+        //fn seat_at_table() {}
     }
 
-    mod serving {
+    /*mod serving {
         fn take_order() {}
 
         fn serve_order() {}
 
         fn take_payment() {}
-    }
+    }*/
+}
+
+pub fn eat_at_a_restaurant() {
+    // absolute path
+    crate::front_of_house::hosting::add_to_wait_list(true);
+
+    //relative path
+    front_of_house::hosting::add_to_wait_list(false);
 }
---
+++ projects/hello_packages/restaurant/src/main.rs
@@ -0,0 +1,8 @@
+
+// Love it
+use restaurant::eat_at_a_restaurant;
+
+fn main() {
+    println!("Hello, world!");
+    eat_at_a_restaurant();
+}
